35867d797b9b5b8ef2be217a4ec815f7a20b5f88
M.G <624586383@qq.com>
2016-05-11 15:53:09 +0800

    Initial commit

---
+++ app/src/main/java/com/feidian/george/hzaumooc/Activity/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.feidian.george.hzaumooc.Activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.feidian.george.hzaumooc.R;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+/**
+ * Created by hy on 2016/5/11.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setStatus();
+    }
+    /**
+     * 设置状态栏背景
+     */
+    private void setStatus() {
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            Window window = getWindow();
+            WindowManager.LayoutParams attributes = window.getAttributes();
+            attributes.flags |= WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            window.setAttributes(attributes);
+            SystemBarTintManager systemBarTintManager = new SystemBarTintManager(this);
+            systemBarTintManager.setStatusBarTintEnabled(true);
+            systemBarTintManager.setStatusBarTintResource(R.color.statusbar_bg);
+        }
+    }
+
+}
